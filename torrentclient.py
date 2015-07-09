dec5f478150239a64d4d39445a9b0940caf19532
Vivekraj <vivek.raj.n7@gmail.com>
2015-07-09 18:52:36 +0530

    Update torrentclient.py

--- torrentclient.py
+++ torrentclient.py
@@ -1,24 +1,24 @@
-import libtorrent as lt
+import libtorrent as lbt
 import time
 import sys
-
-ses = lt.session()
-ses.listen_on(6000, 6200)
-
-info = lt.torrent_info(sys.argv[1])
+ 
+ses = lbt.session()
+ses.listen_on(4000, 4001)
+ 
+info = lbt.torrent_info(sys.argv[1])
 h = ses.add_torrent({'ti': info, 'save_path': './'})
 print 'starting', h.name()
-
+ 
 while (not h.is_seed()):
    s = h.status()
-
-   state_str = ['queued', 'checking', 'downloading metadata', \
-      'downloading', 'finished', 'seeding', 'allocating', 'checking fastresume']
-   print '\r%.2f%% complete (down: %.1f kb/s up: %.1f kB/s peers: %d) %s' % \
+ 
+   state_str = ['Queued', 'Checking', 'Metadata', \
+      'Downloading', 'Completed', 'Seeding', 'Allocating', 'Checking resume']
+   print '\r%.2f%% complete (down: %.1f kb/s up: %.1f kB/s peers: %d seeds: %d ) %s' % \
       (s.progress * 100, s.download_rate / 1000, s.upload_rate / 1000, \
-      s.num_peers, state_str[s.state]),
+      s.num_peers,s.num_seeds, state_str[s.state]),
    sys.stdout.flush()
-
+ 
    time.sleep(1)
-
-print h.name(), 'complete'
+ 
+print h.name(), 'Complete'
